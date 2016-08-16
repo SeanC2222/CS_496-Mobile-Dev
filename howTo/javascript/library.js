721c767272479e066d1cd22d1d8f277a5bbd7a92
Sean Mulholland <mulholls@flip2.engr.oregonstate.edu>
2016-08-16 12:23:14 -0700

    8/16/16: Added How To: Google's Volley Library source

---
+++ howTo/javascript/library.js
@@ -0,0 +1,9 @@
+var prev = document.getElementById("prev_page");
+prev.addEventListener('click', function(){
+   window.location = 'intro.html'
+});
+
+var next = document.getElementById("next_page");
+next.addEventListener('click', function(){
+   window.location = 'software.html'
+});
